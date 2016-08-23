28fbb5c63ce9e944962e644272ec35709111903f
mattgrobelny <mattgrobelny@gmail.com>
2016-08-22 20:54:23 -0500

    Starting Cheat sheet

---
+++ Cheat_sheet.py
@@ -0,0 +1,7 @@
+###############################################################################
+# Printing variable within a string
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
