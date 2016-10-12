4761abac205e830b23ec7cc3824fec8e96237622
Matt Grobelny <mattgrobelny@gmail.com>
2016-10-12 14:46:24 -0500

    Update getopts_demo.py

--- getopts_demo.py
+++ getopts_demo.py
@@ -4,7 +4,7 @@ import sys
 import getopt
 
 # define variables and defaults
-kmer = 0
+kmer = 10
 file_name = ""
 
 # pass in commmands line args array
@@ -18,12 +18,15 @@ except getopt.GetoptError:
     print 'kmer.py -k <kmer_size> -f <inputfile>'
     sys.exit(2)
 for opt, arg in opts:
+    # print output for help
     if opt == '-h':
         print 'kmer.py -k <kmer_size> -f <inputfile>'
         sys.exit()
+    # parse the -k command line parameter 
     elif opt in ("-k"):
-        kmer = arg
+        kmer = arg # make command line input for -k  equal to var kmer
+    # parse the -f command line parameter
     elif opt in ("-f"):
-        file_name = arg
+        file_name = arg # make commandline input for -f equal to var file_name
 print "Kmer size is:", kmer
 print "Input file is:", file_name
